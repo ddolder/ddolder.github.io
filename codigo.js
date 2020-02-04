74a9cf3525a66211425e87b9c5efa7e5923261a7
ddolder <danisadolder@gmail.com>
2020-02-04 14:36:25 -0700

    mm

---
+++ codigo.js
@@ -0,0 +1,10 @@
+  function unitconverter() {
+  var myNum = document.getElementById('inputNumber')
+
+if ("Input Units"=="Inches"){
+
+}
+
+
+document.getElementById('outputNumber').innerHTML= myNum
+}
