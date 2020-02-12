29a0430d8a1a833576f6447c58777420ba3eb69d
ddolder <danisadolder@gmail.com>
2020-02-11 18:34:41 -0700

    Alto y ancho

--- manning.js
+++ manning.js
@@ -7,12 +7,14 @@ function manning() {
     else if(document.getElementById("k2").checked==true){
     k = 1.0;
         }
-    var A = document.getElementById("A").value;
-    var Pw = document.getElementById("Pw").value;
+    var b = document.getElementById("b").value;
+    var h = document.getElementById("h").value;
     var S = document.getElementById("S").value;
-    var velocity = (k/n)*(A/Pw)^(2/3)*(S)^(1/2);
+    var A = b*h;
+    var Pw = b+2*h;
+    var vel = (k/n)*(A/Pw)^(2/3)*(S)^(1/2);
 
-	document.getElementById("calculatednumber").innerHTML = velocity.toPrecision(6);
+	document.getElementById("calculatednumber").innerHTML = vel.toPrecision(6);
 }
 
 function english() {
